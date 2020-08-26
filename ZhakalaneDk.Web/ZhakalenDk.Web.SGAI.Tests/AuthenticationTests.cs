21e6af0aaf9a90cabbec770ce8e2ee4bc7411789
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2020-08-26 17:56:41 +0200

    Authentication (User Pop-Interaction) should be done.
    + Unit Tests

---
+++ ZhakalaneDk.Web/ZhakalenDk.Web.SGAI.Tests/AuthenticationTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZhakalenDk.Web.SGAI.Authentication;
+
+namespace ZhakalenDk.Web.SGAI.Tests.Auth
+{
+    [TestClass]
+    public class AuthenticationTests
+    {
+        Authenticator auth = new Authenticator("credentials.json", "Unit Test Application");
+        string tokenPath = "Tokens";
+
+        [TestMethod]
+        public void AuthAndStore ()
+        {
+            if ( auth == null )
+            {
+                Assert.Fail("Authenticator was null");
+            }
+
+            Assert.IsTrue(auth.AuthAndStoreToken(tokenPath));
+        }
+
+        [TestMethod]
+        public void DeleteResponseTokens ()
+        {
+            if ( auth == null || !auth.AuthAndStoreToken(tokenPath) )
+            {
+                Assert.Fail("Authenticator was null");
+            }
+
+            auth.EraseTokens();
+
+            Assert.IsFalse(File.Exists($"{Assembly.GetExecutingAssembly().Location}/{tokenPath}/Google.Apis.Auth.OAuth2.Responses.TokenResponse-user"));
+
+        }
+
+    }
+}
